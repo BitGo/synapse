1222205d7ccd0920c8969071731676c46f3d9992
Lance R. Vick <lance@lrvick.net>
2020-02-25 20:19:21 -0800

    migrate docker to version locked debian for better supply chain integrity

--- docker/start.py
+++ docker/start.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 
 import codecs
 import glob
@@ -120,7 +120,7 @@ def generate_config_from_template(config_dir, config_path, environ, ownership):
 
     if ownership is not None:
         subprocess.check_output(["chown", "-R", ownership, "/data"])
-        args = ["su-exec", ownership] + args
+        args = ["gosu", ownership] + args
 
     subprocess.check_output(args)
 
@@ -172,10 +172,10 @@ def run_generate_config(environ, ownership):
         # make sure that synapse has perms to write to the data dir.
         subprocess.check_output(["chown", ownership, data_dir])
 
-        args = ["su-exec", ownership] + args
-        os.execv("/sbin/su-exec", args)
+        args = ["gosu", ownership] + args
+        os.execv("/usr/sbin/gosu", args)
     else:
-        os.execv("/usr/local/bin/python", args)
+        os.execv("/usr/bin/python", args)
 
 
 def main(args, environ):
@@ -236,10 +236,10 @@ running with 'migrate_config'. See the README for more details.
 
     args = ["python", "-m", synapse_worker, "--config-path", config_path]
     if ownership is not None:
-        args = ["su-exec", ownership] + args
-        os.execv("/sbin/su-exec", args)
+        args = ["gosu", ownership] + args
+        os.execv("/usr/sbin/gosu", args)
     else:
-        os.execv("/usr/local/bin/python", args)
+        os.execv("/usr/bin/python", args)
 
 
 if __name__ == "__main__":
